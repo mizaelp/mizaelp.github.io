6658fd03e6bd3922dbf7c52ebe1ac53455dc11eb
Mizael Pereira <mizaeludi@outlook.com>
2020-06-04 15:33:22 -0300

    up

--- script.js
+++ script.js
@@ -61,16 +61,54 @@ const notes = [
     }
 ]
 
-document.querySelector('div#app').innerHTML = 
+document.querySelector('a#piano').addEventListener('click', () => {
+    document.querySelector('main#main').innerHTML = `
+        <div class="container_piano">
+        <div class="painel_piano">
+            <div>
+                <label>Volume:</label>
+                <input id="input-volume" type="range" min="0" max="5" step="0.5"/>
+                <span id="volume-value">Valor</span>
+            </div>
+            <select>
+                <option>sine</option>
+                <option>sine</option>
+                <option>sine</option>
+                <option>sine</option>
+            </select>
+        </div>
+        <div class="keys_piano">
+            <button class="button_keys_white">Dó</button>
+            <button class="button_keys_black">Dó#</button>
+            <button class="button_keys_white">Ré</button>
+            <button class="button_keys_black">Ré#</button>
+            <button class="button_keys_white">Mi</button>
+            <button class="button_keys_white">Fá</button>
+            <button class="button_keys_black">Fá#</button>
+            <button class="button_keys_white">Sol</button>
+            <button class="button_keys_black">Sol#</button>
+            <button class="button_keys_white">Lá</button>
+            <button class="button_keys_black">Lá#</button>
+            <button class="button_keys_white">Si</button>
+        </div>
+    </div>
     `
-    <header>
-        <!-- <img class="logo" src="https://avatars1.githubusercontent.com/u/46569836?s=460&u=b13c83036193f1465036090faa396fc75c2a1120&v=4" alt="Profile" width="60px"> -->
-        <nav>
-            <ul class="nav_links">
-                <li><a>Home</a></li>
-                <li><a>About</a></li>
-            </ul>
-        </nav>
-        <a class="cta" href="#"><button>Contact</button></a>
-    </header>
-    <main id="main">Teste</main>`
+})
+
+document.querySelector('a#about').addEventListener('click', () => {
+    document.querySelector('main#main').innerHTML = `
+    <div class="card">
+    <img class="logo" src="https://avatars1.githubusercontent.com/u/46569836?s=460&u=b13c83036193f1465036090faa396fc75c2a1120&v=4" alt="Profile">
+  <h1>Mizael Pereira</h1>
+  <p class="title">Web Developer</p>
+  <p>Graduated at Instituto Federal do Triângulo Mineiro</p>
+  <div style="margin: 24px 0;">
+    <a class="a_card" href="#"><i class="fa fa-dribbble"></i></a> 
+    <a class="a_card" href="#"><i class="fa fa-twitter"></i></a>  
+    <a class="a_card" href="#"><i class="fa fa-linkedin"></i></a>  
+    <a class="a_card" href="#"><i class="fa fa-facebook"></i></a> 
+  </div>
+  <p><button class="btn-contact">Contact</button></p>
+</div>
+    `
+})
